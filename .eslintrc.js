a724e8fd372e2b124108b6413cb57c313e7d9973
Араканцев Константин Геннадьевич <k.arakantsev@bft.local>
2021-12-14 12:39:12 +0300

    setup ts / eslint / prettier

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    // Specifies TypeScript parser as ESLint parser
+    parser: "@typescript-eslint/parser",
+
+    parserOptions: {},
+
+    env: {
+        browser: true,
+        es2021: true,
+    },
+
+    extends: [
+        // Basic ESLint rules
+        "eslint:recommended",
+        // Rules for React
+        "plugin:react/recommended",
+        // Rules for React Hooks
+        "plugin:react-hooks/recommended",
+        // Rules for TypeScript
+        "plugin:@typescript-eslint/recommended",
+        // eslint-config-prettier: turns off ESLint rules that conflict with Prettier formatting rules (implicit and explicit in .prettierrc)
+        // eslint-plugin-prettier: Prettier rules (implicit and explicit in .prettierrc) are used as ESLint rules
+        // Using ESLint with `--fix` option runs Prettier under the hood to format code according its rules
+        // Should be the last config in extends array
+        "plugin:prettier/recommended",
+    ],
+
+    plugins: [],
+
+    rules: {},
+
+    settings: {
+        react: {
+            // Tells eslint-plugin-react to automatically detect the version of React to use
+            version: "detect",
+        },
+    },
+
+    overrides: [
+        {
+            // Set `env: node` for .eslintrc only
+            files: [".eslintrc.js"],
+            env: {
+                node: true,
+            },
+        },
+    ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true,
+    trailingComa: "none",
+    singleQuote: false,
+    printWidth: 120,
+    tabWidth: 4,
+};
